6297ebb9373ddedf092bb64611e85c246216109b
Sukriti <sukriti1394@gmail.com>
2020-02-26 18:45:49 -0800

    adding colour_filters script with documentation

---
+++ R/colour_filters.R
@@ -0,0 +1,18 @@
+# Created on February 26, 2020
+# author: Sukriti Trehan
+# Implementation of colour_filters function in the pymagine package.
+
+
+#' Apply colour filter to given image
+#'
+#' Returns the given image with the user-specified color filter applied.
+#'
+#' @param image string: The local file path for image to which filter will be applied
+#' @param tone string: Colour filter to be applied to the image. Options: 'sepia', 'grayscale', 'rose-tone', 'blue-tone'. Default: 'sepia'
+#'
+#' @return image: image returned with desired colour filter applied
+#' @export
+
+colour_filters <- function(image, tone="sepia"){
+
+}
