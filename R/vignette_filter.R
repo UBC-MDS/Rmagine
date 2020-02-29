ada0fe235a730c539b74c6f9ab666b50e2992c70
Brendon Campbell <brendon.d.campbell@gmail.com>
2020-02-28 16:00:17 -0800

    Added docstring and placeholder function for vignette effect

---
+++ R/vignette_filter.R
@@ -0,0 +1,17 @@
+# Created on February 28, 2020
+# author: Brendon Campbell
+# Implementation of vignette_filter function in the Rmagine package.
+
+#' Apply a vignetting filter to a given image
+#'
+#' Returns the given image with the vignette filter applied at the specified strength.
+#'
+#' @param image string: The local file path for the image to which the filter will be applied.
+#' @param a double: parameter for the strength of the dimming effect. Default: 1.0.
+#'
+#' @return image: image returned with desired distortion applied.
+#' @export
+
+vignette_filter <- function(image, a=1.0) {
+
+}
