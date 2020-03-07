b38bc9d0de96eadc144fef68f1f332ce097793ba
Trevor <tktrevorkwan@gmail.com>
2020-03-06 17:16:28 -0800

    edited edge detection tests

--- tests/test_edge_detection.R
+++ tests/test_edge_detection.R
@@ -4,16 +4,16 @@ test_not_image <- "../imgs/coronado_beach.svg"
 test_image_url <- "www.image.com"
 
 test_that("Filepath must be a string", {
-  expect_error(tunnel_filter(4), 
+  expect_error(edge_detection(3), 
                "Image file path must be a string")
 })
 
 test_that("Filepath must lead to an image", {
-  expect_error(tunnel_filter(test_not_image),
+  expect_error(edge_detection(test_not_image),
                "Image file path should lead to an image")
 })
 
 test_that("Filepath must not be a URL", {
-  expect_error(tunnel_filter(test_image_url),
+  expect_error(edge_detection(test_image_url),
                "Image file path must be in a local directory")
 })
