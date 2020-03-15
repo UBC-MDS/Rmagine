1e7fdc385ab80a1da449a6a697343527187159d4
Sukriti <sukriti1394@gmail.com>
2020-03-14 17:20:22 -0700

    updated NAMESPACE file

--- R/edge_detection.R
+++ R/edge_detection.R
@@ -2,6 +2,10 @@
 # author: Trevor Kwan
 # Implementation of edge_detection function in the Rmagine package.
 
+require(imager)
+require(base)
+utils::globalVariables(c("x","y"))
+utils::globalVariables(c("xy"))
 
 #' Apply edge detection to given image
 #'
@@ -10,50 +14,45 @@
 #' @param image_path string: The local file path for an image of png, jpg, or jpeg format to which edge detection filter will be applied.
 #'
 #' @return array: image array with edge_detection filter applied.
-#' @export 
-#' @example 
+#' @export
+#' @example
 #' edge_detection("imgs/pic.jpg")
 
-require(imager)
-require(base)
-utils::globalVariables(c("x","y"))
-utils::globalVariables(c("xy"))
-
 edge_detection <- function(image_path){
-  
+
   # Exception handling
   if(!is.character(image_path)) {
     stop("Image file path must be a string")
   }
-  
+
   if (startsWith(image_path, "http") | startsWith(image_path, "www")){
     stop("Image file path can't be a URL, provide a local file path.")
   }
-  
+
   if (!endsWith(image_path, ".png") & !endsWith(image_path, ".jpeg") & !endsWith(image_path, ".jpg")){
     stop("Image format must be jpg or jpeg.")
   }
-  
+
   # load greyscale image
   image <- imager::load.image(image_path)
-  
+
   # convert image into grayscale image
   image <- imager::grayscale(image)
-  
+
   # compute an image gradient
   gradient <- imager::imgradient(image,"xy")
-  
+
   # compute the gradient magnitude
   magnitude <- base::with(gradient,sqrt(x^2+y^2))
-  
+
   # take the threshold of the magnitude and convert it to an image
   image <- imager::as.cimg(imager::threshold(magnitude))
-  
+
   # save image as edge_detection_image.jpg
   imager::save.image(image, "edge_detection_image.jpg")
-  
+
   print("The filtered image has been saved to the working directory")
-  
+
   return(image)
-  
+
 }
